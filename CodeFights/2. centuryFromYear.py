6430cd069499e40f633ebcc70070cfccaa4d451c
Demond Love <38409332+DemondLove@users.noreply.github.com>
2020-03-14 13:11:14 -0500

    Update 2. centuryFromYear.py

--- CodeFights/2. centuryFromYear.py
+++ CodeFights/2. centuryFromYear.py
@@ -24,29 +24,7 @@ The number of the century the year is in.
 '''
 
 def centuryFromYear(year):
-    dict = {
-            '1': list(range(1,101))
-            , '2': list(range(101,201))
-            , '3': list(range(201,301))
-            , '4': list(range(301,401))
-            , '5': list(range(401,501))
-            , '6': list(range(501,601))
-            , '7': list(range(601,701))
-            , '8': list(range(701,801))
-            , '9': list(range(801,901))
-            , '10': list(range(901,1001))
-            , '11': list(range(1001,1101))
-            , '12': list(range(1101,1201))
-            , '13': list(range(1201,1301))
-            , '14': list(range(1301,1401))
-            , '15': list(range(1401,1501))
-            , '16': list(range(1501,1601))
-            , '17': list(range(1601,1701))
-            , '18': list(range(1701,1801))
-            , '19': list(range(1801,1901))
-            , '20': list(range(1901,2001))
-            , '21': list(range(2001,2006))
-        }
-    for x in dict.keys():
-        if year in dict[x]:
-            return int(x)
+    if year%100 == 0:
+        return year/100
+    else:
+        return int(year/100)+1    
