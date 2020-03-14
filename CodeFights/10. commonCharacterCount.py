417ced23f203a344d1960bf0925d919e941c96b3
Demond Love <38409332+DemondLove@users.noreply.github.com>
2020-03-14 16:38:23 -0500

    Create 10. commonCharacterCount.py

---
+++ CodeFights/10. commonCharacterCount.py
@@ -0,0 +1,31 @@
+'''
+Given two strings, find the number of common characters between them.
+Example
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+commonCharacterCount(s1, s2) = 3.
+Strings have 3 common characters - 2 "a"s and 1 "c".
+Input/Output
+[execution time limit] 4 seconds (py3)
+[input] string s1
+A string consisting of lowercase English letters.
+Guaranteed constraints:
+1 ≤ s1.length < 15.
+[input] string s2
+A string consisting of lowercase English letters.
+Guaranteed constraints:
+1 ≤ s2.length < 15.
+[output] integer
+'''
+
+def commonCharacterCount(s1, s2):
+    li1 = [x for x in s1]
+    li2 = [x for x in s2]
+    
+    cnt = 0
+    
+    for x in li1:
+        if x in li2:
+            cnt+=1
+            li2.remove(x)
+            
+    return cnt
