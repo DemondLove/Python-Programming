f4045dbc6537ce20b29430e03c1252c16a653021
Demond Love <38409332+DemondLove@users.noreply.github.com>
2020-03-15 05:41:37 -0500

    Create 22. avoidObstacles.py

---
+++ CodeFights/22. avoidObstacles.py
@@ -0,0 +1,38 @@
+'''
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+avoidObstacles(inputArray) = 4.
+
+Check out the image below for better understanding:
+
+
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Non-empty array of positive integers.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 1000,
+1 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The desired length.
+'''
+
+c = 2
+while True:
+    if sorted([i%c for i in inputArray])[0]>0:
+        return c
+    c += 1
