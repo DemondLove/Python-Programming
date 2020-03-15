cb5e689c29c054afa4bb8a86b79c3cc148021c58
Demond Love <38409332+DemondLove@users.noreply.github.com>
2020-03-15 12:11:38 -0500

    Create 36. arrayMaxConsecutiveSum.py

---
+++ CodeFights/36. arrayMaxConsecutiveSum.py
@@ -0,0 +1,48 @@
+'''
+Given array of integers, find the maximal possible sum of some of its k consecutive elements.
+
+Example
+
+For inputArray = [2, 3, 5, 1, 6] and k = 2, the output should be
+arrayMaxConsecutiveSum(inputArray, k) = 8.
+All possible sums of 2 consecutive elements are:
+
+2 + 3 = 5;
+3 + 5 = 8;
+5 + 1 = 6;
+1 + 6 = 7.
+Thus, the answer is 8.
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Array of positive integers.
+
+Guaranteed constraints:
+3 ≤ inputArray.length ≤ 105,
+1 ≤ inputArray[i] ≤ 1000.
+
+[input] integer k
+
+An integer (not greater than the length of inputArray).
+
+Guaranteed constraints:
+1 ≤ k ≤ inputArray.length.
+
+[output] integer
+
+The maximal possible sum.
+'''
+
+def arrayMaxConsecutiveSum(inputArray, k):
+    maxVal = sum(inputArray[0:k])
+    
+    for i in range(len(inputArray)-(k-1)):
+        
+        curMaxVal = sum(inputArray[i:(i+k)])
+        
+        if curMaxVal > maxVal: maxVal = curMaxVal
+    
+    return maxVal
